5b8adc299ccceeb35ca7803ea3ea4b0a3ebb31bb
Juliana Nielson <nielsonjuliana@gmail.com>
2023-09-06 18:10:10 -0600

    initial commit

---
+++ AI Cooking Assistant/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI Cooking Assistant
+//
+//  Created by Juliana Nielson on 9/6/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AI Cooking Assistant/StepsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StepsViewController.swift
+//  AI Cooking Assistant
+//
+//  Created by Juliana Nielson on 9/6/23.
+//
+
+import UIKit
+
+class StepsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
