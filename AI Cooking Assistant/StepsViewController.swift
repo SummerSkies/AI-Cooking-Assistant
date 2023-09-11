212a0d0d2b601f022800ecc87d9c0251b8dbf2e5
Juliana Nielson <nielsonjuliana@gmail.com>
2023-09-11 10:22:54 -0600

    Stacks work conditionally. Added voice control switches.

--- AI Cooking Assistant/StepsViewController.swift
+++ AI Cooking Assistant/StepsViewController.swift
@@ -13,11 +13,16 @@ class StepsViewController: UIViewController {
     @IBOutlet weak var activityLabel: UILabel!
     
     @IBOutlet weak var ingredientListStack: UIStackView!
+    @IBOutlet weak var ingredientsTitleLabel: UILabel!
     @IBOutlet weak var ingredientsLabel: UILabel!
     
     @IBOutlet weak var stepNumberStack: UIStackView!
     @IBOutlet weak var stepNumberLabel: UILabel!
     @IBOutlet weak var instructionsLabel: UILabel!
+    
+    @IBOutlet weak var voiceControlStack: UIStackView!
+    @IBOutlet weak var voiceControlSwitch: UISwitch!
+    @IBOutlet weak var textToSpeechSwitch: UISwitch!
     
     var indexBeingDisplayed: Int = 0
     let shared = ResponseObject.shared
@@ -25,14 +30,18 @@ class StepsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         NotificationCenter.default.addObserver(self, selector: #selector(self.updateUI), name: NotificationManager.didReceiveNetworkResponse, object: nil)
         instructionsLabel.lineBreakMode = .byWordWrapping // or .byCharWrapping
         instructionsLabel.translatesAutoresizingMaskIntoConstraints = false
-        activityIndicator.isHidden = true
-        activityLabel.isHidden = true
+        
         ingredientListStack.isHidden = true
-        ingredientsLabel.isHidden = true
+        stepNumberStack.isHidden = true
+        
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        
+        voiceControlStack.isHidden = true
     }
     
     deinit {
@@ -41,14 +50,27 @@ class StepsViewController: UIViewController {
     
     @objc func updateUI() {
         if indexBeingDisplayed == 0 {
-            stepNumberLabel.text = "Your generated recipe:"
+            activityIndicator.stopAnimating()
+            activityLabel.isHidden = true
+            
+            voiceControlStack.isHidden = false
+            ingredientListStack.isHidden = false
+            stepNumberStack.isHidden = true
+            ingredientsTitleLabel.text = "Ingredients"
+            ingredientsLabel.text = shared.response[indexBeingDisplayed]
+            
+            //nextOrStartButtonTapped([]) //For testing ease; uncomment to press Start button immediately after load. nextOrStartButtonTapped's sender must be changed to Any.
         } else {
+            ingredientListStack.isHidden = true
+            stepNumberStack.isHidden = false
             stepNumberLabel.text = "Step #\(indexBeingDisplayed)"
+            instructionsLabel.text = shared.response[indexBeingDisplayed]
+            
+            instructionsLabel.sizeToFit()
         }
-        instructionsLabel.text = shared.response[indexBeingDisplayed]
     }
     
-    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    @IBAction func nextOrStartButtonTapped(_ sender: UIButton) {
         if indexBeingDisplayed != shared.response.count - 1 {
             indexBeingDisplayed += 1
             updateUI()
