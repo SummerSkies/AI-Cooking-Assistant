2e194f3b8bd08e6a1ebfee3279d42c464da4e269
ZayJayPlays <116956084+ZayJayPlays@users.noreply.github.com>
2023-09-19 09:13:18 -0600

    Customization

--- AI Cooking Assistant/FormViewController.swift
+++ AI Cooking Assistant/FormViewController.swift
@@ -11,11 +11,23 @@ class FormViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var promptTextField: UITextField!
     @IBOutlet weak var cookButton: UIButton!
+    
+    var promptedStrings = String()
+    var numberOfPeople = String()
+    var badIngredients = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         promptTextField.delegate = self
-        //cookButton.isEnabled = false
+        cookButton.isEnabled = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        promptedStrings.append(promptTextField.text!)
+        promptedStrings.append(numberOfPeople.isEmpty ? "":" for \(numberOfPeople)")
+        promptedStrings.append(badIngredients.isEmpty ? "":" but I can't use \(badIngredients)")
+        
+        print(promptedStrings)
     }
     
     //For testing ease; taps Cook immediately after load
@@ -40,11 +52,16 @@ class FormViewController: UIViewController, UITextFieldDelegate {
     let testObject2 = "1. In a large skillet, heat 1 tablespoon of olive oil over medium heat. \n2. Add the sliced onions and cook until they are caramelized, stirring occasionally. This should take about 10 minutes. \n3. In the same skillet, add the sliced mushrooms and minced garlic. Cook for another 5 minutes until the mushrooms are softened. \n4. Remove the onion and mushroom mixture from the skillet and set aside. \n5. Preheat your grill or stovetop grill pan to medium-high heat. \n6. Divide the ground beef into 4 equal portions and shape them into burger patties. \n7. Season the patties with salt and pepper on both sides. \n8. Brush the grill grates with olive oil to prevent sticking. \n9. Place the burger patties on the grill and cook for about 4-5 minutes per side for medium doneness. \n10. During the last minute of cooking, place a slice of Swiss cheese on each patty to allow it to melt. \n11. Remove the burger patties from the grill and let them rest for a few minutes. \n12. Toast the burger buns on the grill or in a toaster until lightly golden. \n13. Assemble the burgers by placing a patty on the bottom bun. \n14. Top with the onion and mushroom mixture. \n15. Add any optional toppings such as lettuce, tomato, caramelized onions, ketchup, or mustard. \n16. Place the top bun on the burger and serve immediately. \n17. Enjoy your delicious gourmet mushroom Swiss burgers!"
     
     @IBAction func cookButtonTapped(_ sender: Any) {
-        fakeNetworkCall()
+        realNetworkCall()
     }
     
     func realNetworkCall() {
         let shared = ResponseObject.shared
+        
+        promptedStrings.append(promptTextField.text!)
+        promptedStrings.append(numberOfPeople.isEmpty ? "":"for \(numberOfPeople)")
+        promptedStrings.append(badIngredients.isEmpty ? "":"but I can't use\(badIngredients)")
+        
         Task {
             shared.response.removeAll()
             var recipe = UserCreatedRecipe(id: UUID(), ingredientsList: "", steps: "")
@@ -52,7 +69,7 @@ class FormViewController: UIViewController, UITextFieldDelegate {
             // First call to generate just the recipe title
             let titleResponse = await OpenAIService.shared.sendMessage(messages: [
                 Message(id: UUID().uuidString, role: .system, content: "You are a seasoned chef, with years of experience in the culinary world, renowned for your skill in both traditional and innovative cooking techniques. You are guiding users through an interactive cooking experience."),
-                Message(id: UUID().uuidString, role: .user, content: "Generate a recipe title based on this request: \(promptTextField.text!)")
+                Message(id: UUID().uuidString, role: .user, content: "Generate a recipe title based on this request: \(promptedStrings)")
             ])
             
             if let title = titleResponse?.choices[0].message.content {
@@ -100,5 +117,12 @@ class FormViewController: UIViewController, UITextFieldDelegate {
         }
         
         RecipeStorageManager.shared.createCoreDataRecipe(for: UserCreatedRecipe(id: UUID(), title: "test", ingredientsList: testObject1, steps: testObject2))
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toCustomize" {
+            let destination = segue.destination as! CustomizeViewController
+            destination.formView = self
+        }
     }
 }
